8c820e15f1be5ed9a4d39e477bf858b52c04db48
dzcpb72 <DZCPB72@SKDAMBL07650.mb.skoda.vwg>
2013-12-04 15:45:30 +0100

    Pokusy s parserem.

--- TribeWikiParser/TribeWikiParser/src/cz/gelion/tribewiki/parser/TracWikiParser.scala
+++ TribeWikiParser/TribeWikiParser/src/cz/gelion/tribewiki/parser/TracWikiParser.scala
@@ -5,7 +5,50 @@ import scala.util.parsing.combinator.RegexParsers
 
 class TracWikiParser extends RegexParsers {
 
+  def beginLine: Parser[String] = """^""".r ^^ {
+    case s => {
+      println("beginLine: " + s)
+      s
+    }
+  }
   
+  def endLine: Parser[String] = """(.*)$""".r
+  
+  def text : Parser[String] = """.*""".r
+  
+  def h1 = (beginLine ~ "=" ~ text ~ "=")  
+  
+  
+  def it : Parser[String] = """''.*''""".r ^^ {
+    case s =>  {
+      println(String.format("italic:'%s'", s))
+      s
+    }
+  } 
+  
+  
+  def body = (h1 | it | text)*
+  
+  
+}
+
+
+object TracWikiParser extends TracWikiParser with Application {
+  
+  println("Jedeme...")
+  
+  
+  val t = scala.io.Source.fromFile("page1.wiki").mkString
+  
+  println("parsing:\n" + t)
+  
+  parseAll(body, t) match {
+    case Success(lup,_) => {
+      println(lup)
+      
+    }
+    case x => println(x)
+  }
   
   
 }
