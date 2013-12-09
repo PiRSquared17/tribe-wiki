0f2e6050575128316ece7b438b99b95451715057
dzcpb72 <DZCPB72@SKDAMBL07650.mb.skoda.vwg>
2013-12-09 16:44:18 +0100

    Neuspesne pokusy.

--- TribeWikiParser/TribeWikiParser/src/cz/gelion/tribewiki/parser/TracWikiParser.scala
+++ TribeWikiParser/TribeWikiParser/src/cz/gelion/tribewiki/parser/TracWikiParser.scala
@@ -2,35 +2,41 @@ package cz.gelion.tribewiki.parser
 
 import scala.util.parsing.combinator.PackratParsers
 import scala.util.parsing.combinator.RegexParsers
+import java.io.Serializable
 
 class TracWikiParser extends RegexParsers {
 
-  def beginLine: Parser[String] = """^""".r ^^ {
-    case s => {
-      println("beginLine: " + s)
-      s
-    }
-  }
+  override def skipWhitespace = false
   
-  def endLine: Parser[String] = """(.*)$""".r
+  def nl:Parser[String] = """\s*(\r\n|\n)""".r ^^ {_.toString}
   
-  def text : Parser[String] = """.*""".r
+  def ascii ="""[A-Za-z0-9]""".r
   
-  def h1 = (beginLine ~ "=" ~ text ~ "=")  
+  def url = (ascii*) ~ "://" ~ (ascii*);
+  
+  def unicode = """\p{L}""".r
+    
+  def text = """[\p{L}0-9\s\.]*""".r
+    
+  def h1 = """=""" ~ ("""\s+""".r?) ~> text <~  ("""\s+""".r?) ~ """=""" ~ nl
+  
+  def it = """''""" ~> text <~ """''"""
+
+  def inline_element = it | text
+  
+  def inline_text:Parser[String] = (inline_element ~ inline_text) ^^ {_.toString}
+  
+  def line_of_text = inline_text ~ nl
+  
+  def lines_of_text:Parser[String] = (line_of_text  | lines_of_text) ^^ {_.toString}
+  
+  def paragraph = (h1?) ~ (lines_of_text?) | lines_of_text
   
   
-  def it : Parser[String] = """''.*''""".r ^^ {
-    case s =>  {
-      println(String.format("italic:'%s'", s))
-      s
-    }
-  } 
-  
-  
-  def body = (h1 | it | text)*
-  
+  def article = h1 ~ paragraph
   
 }
+  
 
 
 object TracWikiParser extends TracWikiParser with Application {
@@ -42,6 +48,10 @@ object TracWikiParser extends TracWikiParser with Application {
   
   println("parsing:\n" + t)
   
+  println(parseAll(article, t)) 
+    
+  
+  /*
   parseAll(body, t) match {
     case Success(lup,_) => {
       println(lup)
@@ -49,6 +59,6 @@ object TracWikiParser extends TracWikiParser with Application {
     }
     case x => println(x)
   }
-  
+  */
   
 }
